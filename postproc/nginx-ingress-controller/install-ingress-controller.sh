bdf723b53b842cb98fd5b9c81f0abf47af8d54f4
Viktor R <s.pepyako@gmail.com>
2024-05-14 21:30:55 +0200

    Added ingress monitoring

--- postproc/nginx-ingress-controller/install-ingress-controller.sh
+++ postproc/nginx-ingress-controller/install-ingress-controller.sh
@@ -2,4 +2,7 @@
 
 # install ingress controller with NLB
 helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx  --namespace ingress-nginx --create-namespace \
-  --set-string controller.service.annotations."service\.beta\.kubernetes\.io/aws-load-balancer-type"="nlb"
+  --set-string controller.service.annotations."service\.beta\.kubernetes\.io/aws-load-balancer-type"="nlb" \
+  --set controller.metrics.enabled=true \
+  --set-string controller.podAnnotations."prometheus\.io/scrape"="true" \
+  --set-string controller.podAnnotations."prometheus\.io/port"="10254"
